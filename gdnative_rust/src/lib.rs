8db5b3cad15db52ce750d2da1ed366dafe237d82
kevin <me@kvnvelasco.com>
2020-06-26 17:54:46 +0800

    update game name and add base case for lib.rs

--- gdnative_rust/src/lib.rs
+++ gdnative_rust/src/lib.rs
@@ -1,7 +1,11 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+#[macro_use]
+extern crate gdnative;
+
+
+fn init(handle: gdnative::init::InitHandle) {
+
 }
+
+godot_gdnative_init!();
+godot_nativescript_init!(init);
+godot_gdnative_terminate!();
